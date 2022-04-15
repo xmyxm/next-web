ba920109f294b1e9851a349715f2a7cf94fe5c18
myco_xmy <myco_xmy@qq.com>
2022-04-15 10:32:47 +0800

    feat: up

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const next = require("next");
 const Router = require("@koa/router");
 
 const hostname = "localhost";
-const port = parseInt(process.env.PORT, 10) || 4000;
 const dev = process.env.NODE_ENV !== "production";
-
+const port = parseInt(process.env.PORT, 10) || 4000;
 const app = next({ dev, hostname, port });
+
 const handle = app.getRequestHandler();
 
+// nextjs 无法直接处理服务端
+// 数据接口
+// 数据库连接
+// session 状态
+
+// 这里必须启动静态资源服务后才能去启动koa
 app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
